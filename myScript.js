8f382abb6ecdb6763ff13ecacc34765bfd7ad32a
khalil sadi <khalilcode01@gmail.com>
2022-08-03 08:47:52 +0300

    Add game function

--- myScript.js
+++ myScript.js
@@ -1,14 +1,12 @@
 let list = ['rock' , 'paper' , 'scissors'];
-let computerChoice = function getComputerChoice() {
+function getComputerChoice() {
     let randomIndex = Math.floor(Math.random() * list.length);        
     return list[randomIndex];
-};
-console.log(computerChoice());
-let player = prompt("Your turn!").toLowerCase;
+}
 let playerCount = 0, computerCount = 0;
 function playRound(playerSelection,computerSelection) {
     if (playerSelection === "paper" && computerSelection === "rock"){
-    console.log("You win! P2aper beats Rock");
+    console.log("You win! Paper beats Rock");
     playerCount++;    
     }
     else if (playerSelection === "rock" && computerSelection === "paper"){
@@ -38,7 +36,22 @@ function playRound(playerSelection,computerSelection) {
     else if (playerSelection === computerSelection){
         console.log("Draw!");
     }
-    else {
-        console.log("invalid value");
+
+}
+       
+function game (){
+    for(let i=0;i<5;i++){
+        let playerChoice = prompt("Your turn!");
+        playerChoice.toLowerCase;
+        playRound(playerChoice,getComputerChoice());
+        if (playerCount === 5){
+            console.log("You win!");
+            break;
+        }
+        if (computerCount === 5){
+            console.log("You lose!");
+            break;
+        }
     }
 }
+game ();
