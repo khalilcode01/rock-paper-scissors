dfd86f7d2d107305098d5395c4da4485b3bc96fd
khalil sadi <khalilcode01@gmail.com>
2022-08-03 08:07:33 +0300

    Add a shuffle function to get the computer choice

--- myScript.js
+++ myScript.js
@@ -1,1 +1,8 @@
-console.log("Hi World");
+let list = ['rock' , 'paper' , 'scissors'];
+let computerChoice = function getComputerChoice(){
+    let randomIndex = Math.floor(Math.random() * list.length);        
+    return list[randomIndex];
+};
+console.log(computerChoice());
+let player = prompt("Your turn!");
+let 
