f2cb5a756291b028a5eff83ba225e36029c68395
khalil sadi <khalilcode01@gmail.com>
2022-08-27 11:22:00 +0300

    Add Ui to the game

--- myScript.js
+++ myScript.js
@@ -1,3 +1,4 @@
+
 let list = ['rock' , 'paper' , 'scissors'];
 function getComputerChoice() {
     let randomIndex = Math.floor(Math.random() * list.length);        
@@ -6,52 +7,89 @@ function getComputerChoice() {
 let playerCount = 0, computerCount = 0;
 function playRound(playerSelection,computerSelection) {
     if (playerSelection === "paper" && computerSelection === "rock"){
-    console.log("You win! Paper beats Rock");
+        divs.textContent = "You win! Paper beats Rock";
+        container.appendChild(divs);
     playerCount++;    
     }
     else if (playerSelection === "rock" && computerSelection === "paper"){
-        console.log("You lose! Rock beats Paper");
+        divs.textContent = "You lose! Rock beats Paper";
+        container.appendChild(divs);
         computerCount++;
     }
     else if (playerSelection === "scissors" && computerSelection === "paper"){
-        console.log("You win! Scissors beats Paper");
+        divs.textContent = "You win! Scissors beats Paper";
+        container.appendChild(divs);
         playerCount++;
     }
     else if (playerSelection === "paper" && computerSelection === "scissors"){
-        console.log("You lose! Scissors beats Paper");
+        divs.textContent = "You lose! Scissors beats Paper";
+        container.appendChild(divs);
         computerCount++;
     }
     else if (playerSelection === "rock" && computerSelection === "scissors"){
-        console.log("You win! Rock beats Scissors");
+        divs.textContent = "You win! Rock beats Scissors";
+        container.appendChild(divs);
         playerCount++;
     }
     else if (playerSelection === "scissors" && computerSelection === "rock"){
-        console.log("You lose! Rock beats Scissors");
+        divs.textContent = "You lose! Rock beats Scissors";
+        container.appendChild(divs);
         computerCount++;
     }
     else if (playerSelection === "scissors" && computerSelection === "paper"){
-        console.log("You win! Scissors beats paper");
+        divs.textContent = "You win! Scissors beats paper";
+        container.appendChild(divs);
         playerCount++;
     }
     else if (playerSelection === computerSelection){
-        console.log("Draw!");
+        divs.textContent = "Draw!";
+        container.appendChild(divs);
     }
 
 }
-       
+let playerChoice;
+let counter = 1;
 function game (){
-    for(let i=0;i<5;i++){
-        let playerChoice = prompt("Your turn!");
-        playerChoice.toLowerCase;
-        playRound(playerChoice,getComputerChoice());
-        if (playerCount === 5){
-            console.log("You win!");
-            break;
+    playerChoice.toLowerCase;
+    playRound(playerChoice,getComputerChoice());    
+    if(counter <= 5) {
+        alert.textContent = counter;
+        container.appendChild(alert);
+        counter++;
+    }
+    else {
+        if (playerCount > computerCount){
+            divs.textContent = 'you won!';
+            container.appendChild(divs);
+            computerCount = 0;
+            playerCount = 0;
         }
-        if (computerCount === 5){
-            console.log("You lose!");
-            break;
+        else if (computerCount > playerCount){
+            divs.textContent = 'you lost';
+            container.appendChild(divs);
+            computerCount = 0;
+            playerCount = 0;
         }
+        counter = 1;
     }
 }
-game ();
+const container = document.querySelector('#container');
+const btnRock = container.querySelector('.rock');
+const btnPaper = container.querySelector('.paper');
+const btnScissors = container.querySelector('.scissors');
+btnRock.addEventListener('click', () => {
+    playerChoice = 'rock';
+    game ();
+});
+btnPaper.addEventListener('click', () => {
+    playerChoice = 'paper';
+    game ();
+});
+btnScissors.addEventListener('click', () => {
+    playerChoice = 'scissors';
+    game ();
+});
+let divs = document.createElement('div');
+divs.setAttribute('id', 'result');
+let alert = document.createElement('h3');
+alert.setAttribute('class', 'announce');
